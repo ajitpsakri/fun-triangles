9cf2180f028b494c2b45027516d84f6d0f1494f0
ajitpsakri <sakriajit@gmail.com>
2021-08-11 16:03:38 +0530

    feat:button click div appear, like react

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-//nav
-const triangleCheckButton = document.querySelector("#btn-triangle-really");
-const findHypotenusButton = document.querySelector("#btn-find-hypotenus");
-const findAreaButton = document.querySelector("#btn-find-area");
-const quizButton = document.querySelector("#btn-quiz");
 
-//divisions
-const triangleCheck = document.querySelector(".triangle-check");
-const quiz = document.querySelector(".quiz");
-const findHypotenus = document.querySelector(".give-me-hypotenuse");
-const findArea = document.querySelector(".find-area");
-
-const allDiv = [triangleCheck,quiz,findHypotenus,findArea];
-const allButton = [triangleCheckButton,quizButton,findHypotenusButton,findAreaButton]
-
-allButton.map(ele=>displayDivBasedOnButtonClicked(ele))
-
-console.log(Object.keys(triangleCheck))
-function displayDivBasedOnButtonClicked(btnName){
-    
-    btnName.addEventListener('click',function(btnName){
-        allDiv.map(ele=>clear(ele))
-        //convert variable name btnName to string form(Hint use object)
-        console(eval(btnName.replace(/(\w+)Button/g,"$1")))
-        // display(eval(btnName.replace(/(\w+)Button/g,"$1")))
-    })    
+const buttonObj = {
+ triangleCheckButton : document.querySelector("#btn-triangle-really"),
+ findHypotenusButton : document.querySelector("#btn-find-hypotenus"),
+ findAreaButton : document.querySelector("#btn-find-area"),
+ quizButton : document.querySelector("#btn-quiz"),
 }
 
-// const showWhatIsClick = () => {}
+const divObj = {
+ triangleCheck : document.querySelector(".triangle-check"),
+ quiz : document.querySelector(".quiz"),
+ findHypotenus : document.querySelector(".give-me-hypotenuse"),
+ findArea : document.querySelector(".find-area"),
+}
+
+for(let key in buttonObj){
+    displayDivBasedOnButtonClicked(key)
+}
+
+// const allDiv = [triangleCheck,quiz,findHypotenus,findArea];
+// const allButton = [triangleCheckButton,quizButton,findHypotenusButton,findAreaButton]
+
+// allButton.map(ele=>displayDivBasedOnButtonClicked(ele))
+
+function displayDivBasedOnButtonClicked(btnName){
+    buttonObj[btnName].addEventListener('click',function(){
+        clearDivs()   
+        //convert variable name btnName to string form(Hint use object)
+       display(eval(divObj[btnName.replace(/(\w+)Button/g,"$1")]))
+    })    
+}
 const clear = whatToClear => whatToClear.style.display = "none";
 const display= whatToDisplay => whatToDisplay.style.display = "block";
+
+const clearDivs = () => {
+    for(let key in divObj){
+        clear(divObj[key])
+    }
+}
+clearDivs()
+// const showWhatIsClick = () => {}
+
 
